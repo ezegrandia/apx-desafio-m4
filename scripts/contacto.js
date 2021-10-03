fc7d1dfacec10e7ef45eb48b0161622ec6ff6b6d
Ezegrandia <ezequielgrandia@gmail.com>
2021-10-03 18:59:58 -0300

    termino la page de contacto

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -2,8 +2,20 @@ function main() {
   const headerContainerEl = document.querySelector(
     ".header-component-container"
   );
-
   insertHeaderComponent(headerContainerEl);
+
+  const formContainerEl = document.querySelector(".form-component-container");
+  insertFormComponent(formContainerEl);
+
+  const footerContainerEl = document.querySelector(
+    ".footer-component-container"
+  );
+  insertFooterComponent(footerContainerEl);
+
+  const formTitleEl = document.querySelector(".form__title");
+  formTitleEl.textContent = "Contacto";
+
+  showLabelsFormEls();
 }
 
 main();
